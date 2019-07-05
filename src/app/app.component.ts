251df12035370bfbdbfaeeca24323fb67ada13f5
Oris John-Baptiste <orisjb@gmail.com>
2019-07-05 14:47:01 +0100

    made a rudimentary connection to the database.. needs more work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { OilTableService } from './oil-table.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/database';
+//import 'rxjs/add/operator/toPromise';
+
 
 @Component({
   selector: 'app-root',
@@ -13,13 +16,35 @@ import { Observable } from 'rxjs';
 export class AppComponent {
 
   //oilsCollection: AngularFirestoreCollection
-  title = 'oilfinder';
-  tasks: Observable<any[]>;
-  constructor(private db: AngularFirestore, private oilTableService: OilTableService) {
-    
+  title = 'Oil Finder';
+  desc = 'Seeing if we can hook up a backend...'
+  oils: Observable<any[]>;
+  oilValue = '';
+  constructor(public db: AngularFirestore, private oilTableService: OilTableService) {
+    this.oils = db.collection('oils').valueChanges();
   }
 
-  ngOnInit() {
-    this.tasks = this.db.collection('tasks').valueChanges();
+  
+  onSubmit () {
+    this.db.collection("users").add({
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });
+  }
+
+  doTheThing () {
+    console.log('uoo');
+  //   db.collection("users").get().then((querySnapshot) => {
+  //     querySnapshot.forEach((doc) => {
+  //         console.log(`${doc.id} => ${doc.data()}`);
+  //     });
+  // });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -42,13 +43,15 @@ import { environment } from '../environments/environment';
     BrowserModule,
     ReactiveFormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(environment.firebase, 'oil-manager'),
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'oil-manager'),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireDatabaseModule 
   ],
   providers: [OilTableService],
   bootstrap: [AppComponent],
-  exports: [OilDetailComponent]
+  exports: []
 })
 export class AppModule { }
